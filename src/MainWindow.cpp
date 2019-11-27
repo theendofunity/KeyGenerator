8220c816503c57f59d160879583ff3ac097aa77f
dudkin_dv <dudkin_dv@tomsk.lemz.ru>
2019-11-27 14:49:16 +0700

    Initial commit

---
+++ src/MainWindow.cpp
@@ -0,0 +1,17 @@
+#include "MainWindow.h"
+
+#include "QVBoxLayout"
+#include "QLabel"
+#include "QLineEdit"
+#include "QCheckBox"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+
+}
+
+MainWindow::~MainWindow()
+{
+}
+
